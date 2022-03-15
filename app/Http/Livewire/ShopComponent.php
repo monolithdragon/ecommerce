472553f786d6affb1f12272b21e63bc5c4814087
monolithdragon <joo.lajos76@gmail.com>
2022-03-15 17:19:08 +0100

    Product sorting and sizing.

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,6 +10,15 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 class ShopComponent extends Component
 {
     use WithPagination; 
+
+    public $sorting;
+    public $pageSize;
+
+    public function mount()
+    {
+        $this->sorting = 'default';
+        $this->pageSize = 12;
+    }
     
     public function store($id, $name, $price)
     {
@@ -20,7 +29,24 @@ class ShopComponent extends Component
 
     public function render()
     {
-        $products = Product::paginate(12);
+        switch ($this->sorting) {
+            case 'date':
+                $products = Product::orderBy('created_at', 'DESC')->paginate($this->pageSize);
+                break;
+            
+            case 'price':
+                $products = Product::orderBy('price', 'ASC')->paginate($this->pageSize);
+                break;
+
+            case 'price-desc':
+                $products = Product::orderBy('price', 'DESC')->paginate($this->pageSize);
+                break;
+            
+            default:
+                $products = Product::paginate($this->pageSize);
+                break;
+        }
+        
         return view('livewire.shop-component', ['products' => $products]);
     }
 }
